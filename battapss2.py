c7b1e0752b34be0904e740e5c04dfeacf815345a
bua0409 <52872364+bua0409@users.noreply.github.com>
2019-07-27 15:32:57 +0700

    Add files via upload

---
+++ battapss2.py
@@ -0,0 +1,42 @@
+# # from turtle import *
+# # shape ("turtle")
+# # speed(-1)
+# # for i in range (50):
+# #     for i in range (4):
+# #         forward(100)
+# #         left(90)
+# #     left (10)    
+# # mainloop()    
+
+
+
+
+
+# # # from turtle import *
+# # # shape ("turtle")
+# # # speed(-1)
+# # # for i in range (50):
+# # #     for i in range (4):
+# # #       forward (100)
+# # #       left (90)
+# # #     left (1s0)
+# # # mainloop()
+
+
+
+
+# # # from turtle import * 
+# # # from turtle import *
+# # # color("red")
+# # # speed(-1)
+# # # for i in range(10):
+# # #     circle(100)
+# # #     left (36) 
+# # # mainloop()
+# from turtle import *
+# color("red")
+# speed(-1)
+# for i in range(50):
+#     circle(70)
+#     left(10)
+# mainloop()    
